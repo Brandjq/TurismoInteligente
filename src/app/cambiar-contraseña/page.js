bf4a7a1b8167783464c43363e1a8037cd8253bf0
Brandon <brandonjnajera0803@gmail.com>
2025-09-22 10:39:07 -0600

    slucion error contraseña

---
+++ src/app/cambiar-contraseña/page.js
@@ -0,0 +1,8 @@
+export default function CambiarContraseña() {
+  return (
+    <div style={{padding:40, textAlign:'center'}}>
+      <h2>Cambiar Contraseña</h2>
+      <p>Página en construcción.</p>
+    </div>
+  );
+}
